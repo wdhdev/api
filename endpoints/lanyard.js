0a2185bdcffdf465ec0c6c01bd4257f370685f19
William Harrison <william@williamdavidharrison.com.au>
2023-06-10 09:47:30 +0800

    Update lanyard.js
    - Fix discriminator showing for pomelo usernames
    - Update user not monitored status code

--- endpoints/lanyard.js
+++ endpoints/lanyard.js
@@ -3,18 +3,18 @@ module.exports = async (req, res) => {
 
     const request = await fetch(`https://api.lanyard.rest/v1/users/${id}`).then((res) => res.json());
 
-    if(!request.success && request.error.code === "user_not_monitored") return res.status(500).json({ "error": "USER_NOT_MONITORED", "message": "https://discord.gg/7BxbX8jKtb" });
+    if(!request.success && request.error.code === "user_not_monitored") return res.status(400).json({ "error": "USER_NOT_MONITORED", "message": "https://discord.gg/7BxbX8jKtb" });
 
     res.status(200).json({
         "user": {
             "profile": {
                 "avatar": {
-                    "id": request.data.discord_user.avatar,
+                    "hash": request.data.discord_user.avatar,
                     "decoration": request.data.discord_user.avatar_decoration
                 },
                 "display_name": request.data.discord_user.display_name,
                 "username": request.data.discord_user.username,
-                "discriminator": request.data.discord_user.discriminator.endsWith("#0") ? null : request.data.discord_user.discriminator,
+                "discriminator": request.data.discord_user.discriminator === "0" ? null : request.data.discord_user.discriminator,
                 "pomelo_username": request.data.discord_user.discriminator === "0" ? true : false
             },
             "id": request.data.discord_user.id,
