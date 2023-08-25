20eeba2a4c5275083e33b1f1b8b4d3a721523863
William Harrison <william@williamdavidharrison.com.au>
2023-08-25 08:55:11 +0800

    Update gravatar.ts

--- src/endpoints/gravatar.ts
+++ src/endpoints/gravatar.ts
@@ -2,14 +2,14 @@ import { Request, Response } from "express";
 import gravatarUrl from "gravatar-url";
 
 export default async (req: Request, res: Response) => {
-    const email: any = req.query.email;
+    const { email } = req.body;
 
     if(!email) return res.status(400).send({ message: "No email specified.", code: "NO_EMAIL" });
     if(!validateEmail(email)) return res.status(400).send({ message: "Invalid email specified.", code: "INVALID_EMAIL" });
 
-    const gravatarURL = gravatarUrl(email);
+    const gravatarURL = gravatarUrl(email.toLowerCase());
 
-    res.status(200).json({ email: email, hash: gravatarURL.replace("https://gravatar.com/avatar/", ""), url: gravatarURL });
+    res.status(200).json({ email: email.toLowerCase(), hash: gravatarURL.replace("https://gravatar.com/avatar/", ""), url: gravatarURL });
 }
 
 function validateEmail(input: string): boolean {
