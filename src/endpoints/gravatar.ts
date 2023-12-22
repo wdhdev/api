4d43e5cfcbe4b62418eae2baabfa2a5ce0ca2562
William Harrison <william@williamdavidharrison.com.au>
2023-12-22 10:48:13 +0800

    feat: remove gravatar rate limit + use william.js

--- src/endpoints/gravatar.ts
+++ src/endpoints/gravatar.ts
@@ -1,19 +1,14 @@
 import { Request, Response } from "express";
-import gravatarUrl from "gravatar-url";
+
+import { gravatar, validate } from "william.js";
 
 export default async (req: Request, res: Response) => {
     const email = req.query.email as string;
 
     if(!email) return res.status(400).send({ message: "No email specified.", code: "NO_EMAIL" });
-    if(!validateEmail(email)) return res.status(400).send({ message: "Invalid email specified.", code: "INVALID_EMAIL" });
+    if(!validate.email(email)) return res.status(400).send({ message: "Invalid email specified.", code: "INVALID_EMAIL" });
 
-    const gravatarURL = gravatarUrl(email.toLowerCase());
+    const gravatarURL = gravatar(email.toLowerCase());
 
     res.status(200).json({ email: email.toLowerCase(), hash: gravatarURL.replace("https://gravatar.com/avatar/", ""), url: gravatarURL });
-}
-
-function validateEmail(input: string): boolean {
-    const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-    return input.match(regex) ? true : false;
 }
--- src/util/router.ts
+++ src/util/router.ts
@@ -5,13 +5,6 @@ import routes from "./routes";
 
 import rateLimit from "express-rate-limit";
 
-const limiter = rateLimit({
-	windowMs: 60 * 60 * 1000, // 60 minutes
-	max: 240, // 240 requests
-	standardHeaders: true,
-	legacyHeaders: false
-})
-
 const formLimiter = rateLimit({
 	windowMs: 24 * 60 * 60 * 1000, // 24 hours
 	max: 2, // 2 requests
@@ -31,7 +24,7 @@ router.post("/forms/contact", formLimiter, async (req: Request, res: Response) =
     routes.forms.contact(req, res);
 })
 
-router.get("/gravatar", limiter, async (req: Request, res: Response) => {
+router.get("/gravatar", async (req: Request, res: Response) => {
     routes.gravatar(req, res);
 })
 
